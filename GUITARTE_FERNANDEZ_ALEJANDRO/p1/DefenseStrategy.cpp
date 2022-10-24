dfaebf0bfabf5160c640898b22aa28792e31473b
Alejandro <alexguitarte@gmail.com>
2022-10-24 13:58:28 +0200

    Avanzada funcion de factibilidad

---
+++ GUITARTE_FERNANDEZ_ALEJANDRO/p1/DefenseStrategy.cpp
@@ -0,0 +1,82 @@
+// ###### Config options ################
+
+//#define PRINT_DEFENSE_STRATEGY 1    // generate map images
+
+// #######################################
+
+#define BUILDING_DEF_STRATEGY_LIB 1
+
+#include "../simulador/Asedio.h"
+#include "../simulador/Defense.h"
+
+#ifdef PRINT_DEFENSE_STRATEGY
+#include "ppm.h"
+#endif
+
+#ifdef CUSTOM_RAND_GENERATOR
+RAND_TYPE SimpleRandomGenerator::a;
+#endif
+
+using namespace Asedio;
+
+float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
+	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
+	return 0; //Esta es la que asigna valor para el centro de extraccion, crear otra para el resto
+}
+
+bool factible(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
+	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*>::iterator def, List<Defense*> defenses){
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    bool res = true;
+    if(row < 0 || col < 0 || row >= nCellsHeight || col >= nCellsWidth || !freeCells[row][col])
+        res = false;
+    else{
+        auto d = defenses.begin();
+        while(d != def && res){  //Las defensas se isertan desde 0 de una en una, por lo que las siguientes a i no estan colocadas
+            Vector3 v(col * cellWidth + cellWidth / 2, row * cellHeight + cellHeight / 2, 0);
+            if(corta((*d)->position, v, (*d)->radio, (*def)->radio)) 
+                res = false;
+        }
+    }
+}
+
+void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight
+              , std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+
+    int maxAttemps = 1000;
+    List<Defense*>::iterator currentDefense = defenses.begin();
+    while(currentDefense != defenses.end() && maxAttemps > 0) {
+
+        (*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
+        (*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
+        (*currentDefense)->position.z = 0; 
+        ++currentDefense;
+    }
+
+#ifdef PRINT_DEFENSE_STRATEGY
+
+    float** cellValues = new float* [nCellsHeight]; 
+    for(int i = 0; i < nCellsHeight; ++i) {
+       cellValues[i] = new float[nCellsWidth];
+       for(int j = 0; j < nCellsWidth; ++j) {
+           cellValues[i][j] = ((int)(cellValue(i, j))) % 256;
+       }
+    }
+    dPrintMap("strategy.ppm", nCellsHeight, nCellsWidth, cellHeight, cellWidth, freeCells
+                         , cellValues, std::list<Defense*>(), true);
+
+    for(int i = 0; i < nCellsHeight ; ++i)
+        delete [] cellValues[i];
+	delete [] cellValues;
+	cellValues = NULL;
+
+#endif
+}
+
+bool corta(Vector3 pos1, Vector3 pos2, float r1, float r2){
+    return r1 + r2 > sqrt(pow(pos1.x - pos2.x, 2) + pow(pos1.y - pos2.y, 2));
+}
