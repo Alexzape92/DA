290a3ca4cbb21c98053a2b79de593fb6887403ca
Alejandro <alexguitarte@gmail.com>
2022-11-14 13:47:23 +0100

    Terminada la p2, falta optimizar

--- GUITARTE_FERNANDEZ_ALEJANDRO/p2/DefensesSelection.cpp
+++ GUITARTE_FERNANDEZ_ALEJANDRO/p2/DefensesSelection.cpp
@@ -11,7 +11,7 @@
 using namespace Asedio;
 
 float value(Defense* def){
-    return def->damage + def->attacksPerSecond + def->dispersion + def->health + def->radio;
+    return def->damage/def->attacksPerSecond + 0*def->dispersion + 0.6*def->health + 0.1*def->range;
 }
 
 int indice(std::list<Defense*>::const_iterator i, const std::list<Defense*>& defs){
@@ -23,7 +23,15 @@ int indice(std::list<Defense*>::const_iterator i, const std::list<Defense*>& def
     return index;
 }
 
-void matriz(float** mat, int c, const std::list<Defense*>& defenses){
+Defense* defensa(int indice, const std::list<Defense*> defenses){
+    auto d = defenses.begin();  //indice 0
+    for(int i = 0; i < indice; i++)
+        d++;
+    
+    return (*d);
+}
+
+void matriz(float** mat, unsigned int c, const std::list<Defense*>& defenses){
     int n = defenses.size();
     auto i = defenses.begin();
 
@@ -45,12 +53,35 @@ void matriz(float** mat, int c, const std::list<Defense*>& defenses){
     }
 }
 
+void recuperar(float** mat, std::list<int>& resultado, int n, int c, const std::list<Defense*>& defenses){
+    int i = n-1, j = c;
+
+    while(i != 0){
+        if(mat[i][j] != mat[i-1][j]){
+            resultado.push_front(defensa(i, defenses)->id);
+            j -= defensa(i,defenses)->cost;
+        }
+        i--;
+    }
+    
+    if(j >= (*defenses.begin())->cost)
+        resultado.push_front((*defenses.begin())->id);
+}
+
 void DEF_LIB_EXPORTED selectDefenses(std::list<Defense*> defenses, unsigned int ases, std::list<int> &selectedIDs
             , float mapWidth, float mapHeight, std::list<Object*> obstacles) {
 
-    unsigned int cost = 0;
-    std::list<Defense*>::iterator it = defenses.begin();
-    while(it != defenses.end()) {
-        
-    }
+    int n = defenses.size();
+    unsigned int c = ases - (*defenses.begin())->cost;  //Restamos el coste de la mina
+
+    selectedIDs.push_front((*defenses.begin())->id);
+    defenses.pop_front();   //No debemos tener en cuenta la mina
+    n--;
+
+    float** tabla = new float*[n];
+    for(int i = 0; i < n; i++)
+        tabla[i] = new float[c+1];
+
+    matriz(tabla, c, defenses);  //Rellenamos la tabla
+    recuperar(tabla, selectedIDs, n, c, defenses); //Guardamos en selectedIDs las IDs seleccionadas
 }
